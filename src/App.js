88589f5b18cd47be097f268eb0a6feab2d2f8b1d
Hsiao Yao <tomtogo1260@gmail.com>
2020-12-11 14:43:55 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="dust">
+        <div className="eye left"></div>
+        <div className="eye right"></div>
+      </div>
+      <footer>
+        A fun pure CSS exercise for me to make animation on the browser.
+      </footer>
+    </>
+  );
+}
+
+export default App;
